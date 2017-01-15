efc5cf22ab42a2169f8c21b0685e9e4a3b3e6c04
Gmz <g_merchez@opmbx.org>
2017-01-15 18:58:35 +0200

    Add files via upload
    Adding c++ support.

---
+++ src/main.c
@@ -0,0 +1,51 @@
+#include "inject.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int Inject(DWORD procId, char* dllPath)
+{
+    HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
+    if(handle)
+    {
+        LPVOID LoadLibAddr = (LPVOID)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+        if(LoadLibAddr == NULL)
+        {
+            ERR("Cannot find LoadLibraryA Handle in kernel32.dll.");
+            return -1;
+        }
+
+        LPVOID SpaceAlloc = (LPVOID)VirtualAllocEx(handle, NULL, strlen(dllPath), MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+        if(SpaceAlloc == NULL)
+        {
+            ERR("Cannot allocate enough memory for the specified dll path.");
+            return -1;
+        }
+
+        if(!WriteProcessMemory(handle, (LPVOID)SpaceAlloc, dllPath, strlen(dllPath), NULL))
+        {
+            ERR("Cannot write the dll path into the remote process memory.");
+            return -1;
+        }
+
+        if(CreateRemoteThread(handle, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibAddr, (LPVOID)SpaceAlloc, 0, 0) == NULL)
+        {
+            ERR("Cannot create a new thread into the remote process.");
+            return -1;
+        }
+
+        CloseHandle(handle);
+        VirtualFreeEx(handle, SpaceAlloc, strlen(dllPath), MEM_COMMIT);
+
+        return 0;
+    }
+
+    ERR("Can't open remote process.");
+
+    return -1;
+}
+
+#ifdef __cplusplus
+}
+#endif
