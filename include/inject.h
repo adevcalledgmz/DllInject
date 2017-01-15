4cbf5c426ef104aee2f2345c42d45b152838f33f
Gmz <g_merchez@opmbx.org>
2017-01-15 18:56:55 +0200

    Delete inject.h

--- include/inject.h
+++
@@ -1,9 +0,0 @@
-#ifndef INJECT_H_INCLUDED
-#define INJECT_H_INCLUDED
-
-#include <windows.h>
-#define ERR(msg) MessageBoxA(NULL, msg, "Error", MB_ICONERROR)
-
-int Inject(DWORD procId, char* dllPath);
-
-#endif
